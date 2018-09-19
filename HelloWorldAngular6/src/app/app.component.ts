b6ea0685b808dd7712a951a2ceb0bc23b22c9ed5
Michaël Cloots <michael.cloots@gmail.com>
2018-09-19 12:10:37 +0200

    structural directives

--- HelloWorldAngular6/src/app/app.component.ts
+++ HelloWorldAngular6/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorldAngular6';
+  isActive: boolean = true;
+  studenten: string[] = ["Jos","Louis","Bert"];
 }
