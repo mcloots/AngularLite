127ca0210c6cb8046330e38dd69b71ee6fe6acc8
mcloots <michael.cloots@gmail.com>
2018-11-21 14:35:37 +0100

    prod environment

--- HelloWorldAngular6/src/environments/environment.prod.ts
+++ HelloWorldAngular6/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCVsfri1Xa3X6THnHD0xloaOJxB54Jf5-g",
+    authDomain: "thomasmore-c13b0.firebaseapp.com",
+    databaseURL: "https://thomasmore-c13b0.firebaseio.com",
+    projectId: "thomasmore-c13b0",
+    storageBucket: "thomasmore-c13b0.appspot.com",
+    messagingSenderId: "689821377638"
+  }
 };
